629f59f18ac115a2e72b604b8aedff97039434f5
Phillip Malboeuf <phil@boeuf.coffee>
2018-09-30 11:40:29 -0400

    editor image upload

--- app/scripts/components/editor.tsx
+++ app/scripts/components/editor.tsx
@@ -1,7 +1,8 @@
 
 import * as React from 'react'
-
 import Quill, { Delta, TextChangeHandler } from 'quill'
+
+import { storage, auth } from '../clients/firebase'
 import { withDBContext, DBContextProps } from '../contexts/db'
 
 
@@ -27,11 +28,14 @@ export class Editor extends React.PureComponent<Props, State> {
       theme: 'snow',
       modules: this.props.readOnly ? { toolbar: null } : {
         toolbar: {
-          container: [['bold', 'italic', 'underline', 'strike'], [{ 'align': [] }, { 'indent': '-1'}, { 'indent': '+1' }], ['code-block', 'blockquote', { 'list': 'ordered'}, { 'list': 'bullet' }], ['link', 'image'], ['clean']]
+          container: [['bold', 'italic', 'underline', 'strike'], [{ 'align': [] }, { 'indent': '-1'}, { 'indent': '+1' }], ['code-block', 'blockquote', { 'list': 'ordered'}, { 'list': 'bullet' }], ['link', 'image'], ['clean']],
+          handlers: {
+            image: this.image.bind(this)
+          }
         }
       },
       readOnly: this.props.readOnly,
-      placeholder: this.props.placeholder || '...'
+      placeholder: this.props.placeholder || '...',
     })
     this.handler = ()=> this.props.onChange && this.props.onChange(this.editor.getContents())
     this.editor.on('text-change', this.handler)
@@ -41,6 +45,21 @@ export class Editor extends React.PureComponent<Props, State> {
   componentWillUnmount() {
     this.editor.off('text-change', this.handler)
     delete this.editor
+  }
+
+  public image() {
+    let input = document.createElement('input')
+    input.setAttribute('type', 'file')
+    input.setAttribute('accept', 'image/*')
+    input.click()
+    input.onchange = ()=> this.upload(input.files[0])
+  }
+
+  public upload(file: File) {
+    let ref = storage.ref(`${auth.currentUser.uid}/images/${file.name}`)
+    ref.put(file)
+      .then(snapshot => ref.getDownloadURL())
+      .then(url => this.editor.insertEmbed(this.editor.getSelection().index, 'image', url))
   }
 
   public render() {
